1d41bb1c4964c552c704b3acdea4454533a089b1
fidellim <lim.fidel@gmail.com>
2022-08-03 22:16:10 +0400

    populate todos and add add task modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import { AiOutlineClose, AiFillEdit, AiOutlinePlus } from "react-icons/ai";
+import { BiTask } from "react-icons/bi";
 
 const App = () => {
 	const API_BASE = "http://localhost:5000";
@@ -76,9 +78,65 @@ const App = () => {
 		setTodos(todos.filter((todo) => todo._id !== data.result._id));
 	};
 
+	// submit new task using "Enter" key
+	const handleEnterKey = (e) => {
+		//it triggers by pressing the enter key
+		if (e.keyCode === 13) {
+			addTodo();
+		}
+	};
+
 	return (
 		<main>
 			<h1>Todo App</h1>
+
+			<div className="todos">
+				{todos.map((todo) => (
+					<div className="todo" key={todo._id}>
+						<div className="checkbox"></div>
+						<div className="text">{todo.text}</div>
+						<div className="editTodo">
+							<AiFillEdit />
+						</div>
+						<div className="deleteTodoBtn" onClick={() => deleteTodo(todo._id)}>
+							<AiOutlineClose />
+						</div>
+					</div>
+				))}
+
+				{todos.length === 0 && <p>No tasks</p>}
+			</div>
+
+			<div
+				className="addTodoFloatingBtn"
+				onClick={() => setPopupAddNewTodo(!popupAddNewTodo)}
+			>
+				<AiOutlinePlus />
+			</div>
+
+			{popupAddNewTodo && (
+				<div className="addTodoModal" onClick={() => setPopupAddNewTodo(false)}>
+					<div onClick={(e) => e.stopPropagation()}>
+						<div
+							className="closePopup"
+							onClick={() => setPopupAddNewTodo(false)}
+						>
+							<AiOutlineClose />
+						</div>
+						<h2>Add Task</h2>
+						<input
+							type="text"
+							className="addTodoInput"
+							onChange={(e) => setNewTodo(e.target.value)}
+							value={newTodo}
+							onKeyDown={(e) => handleEnterKey(e)}
+						/>
+						<div className="addTodoBtn" onClick={addTodo}>
+							<BiTask />
+						</div>
+					</div>
+				</div>
+			)}
 		</main>
 	);
 };
